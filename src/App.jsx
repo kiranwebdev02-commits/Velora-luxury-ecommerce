4c7741f28d4c0dad5de6088a2f352aad558dbab1
kiranwebdev02-commits <kiranwebdev02@gmail.com>
2026-07-26 00:37:11 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const ScrollToTop = () => {
 
 function App() {
   return (
-    <Router basename="/Velora-luxury-ecommerce">
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
 
       <div className="app-container">

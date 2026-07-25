12116821cdff30545e830edf3971ac04dd90aff6
kiranwebdev02-commits <kiranwebdev02@gmail.com>
2026-07-26 00:34:33 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Velora-luxury-ecommerce/',
+  base: process.env.NETLIFY === 'true'
+    ? '/'
+    : '/Velora-luxury-ecommerce/',
 })
